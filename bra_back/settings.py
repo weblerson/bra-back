612205ba0a88a8e30bb1890316db264849d95d83
weblerson <zonejr4@gmail.com>
2022-12-12 21:39:39 -0400

    django apps organization

--- bra_back/settings.py
+++ bra_back/settings.py
@@ -14,10 +14,13 @@ from pathlib import Path
 from decouple import config
 
 import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, '../apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
